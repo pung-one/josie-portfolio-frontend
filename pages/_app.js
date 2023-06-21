1130af2f030b3bd7cf8342beb58282c056dbbedb
pung-one <p-ungerer@gmx.net>
2023-06-21 19:07:04 +0200

    add meta tags and favicon to head component in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import GlobalStyle from "@/styles/styles";
+import Head from "next/head";
 import Layout from "@/components/Layout";
 import { useEffect, useState } from "react";
 
@@ -31,6 +32,37 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
+      <Head>
+        <title>Josie Overton Portfolio</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="description"
+          content="Explore the work of artist Josie Overton"
+          key="desc"
+        />
+        <meta property="og:title" content="Josie Overton Portfolio" />
+        <meta
+          property="og:description"
+          content="Explore the work of artist Josie Overton"
+        />
+      </Head>
       <Layout>
         <Component {...pageProps} deviceType={deviceType} />
       </Layout>
